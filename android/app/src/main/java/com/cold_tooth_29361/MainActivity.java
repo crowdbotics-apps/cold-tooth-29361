e464c93dce6666b1c1c1931ee973124e0ca89050
Crowdbotics <team@crowdbotics.com>
2021-08-03 00:48:14 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/cold_tooth_29361/MainActivity.java
@@ -0,0 +1,15 @@
+package com.cold_tooth_29361;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "cold_tooth_29361";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "cold_tooth_29361Identifier",
+  url: "https://cold_tooth_29361Identifier.botics.co",
+  api: "https://cold_tooth_29361Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
